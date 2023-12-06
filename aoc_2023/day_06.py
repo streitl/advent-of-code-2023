851882541141296f26deb38fd3a4a572b6aa4ebb
Lou Streit <lou.streit@proton.me>
2023-12-06 10:32:01 +0100

    feat: day 06

---
+++ aoc_2023/day_06.py
@@ -0,0 +1,56 @@
+from functools import reduce
+from dataclasses import dataclass
+
+import numpy as np
+
+from aoc_2023.utils import load_input
+
+
+type Races = tuple[Race, ...]
+
+
+@dataclass
+class Race:
+    time: int
+    distance_record: int
+
+
+def get_numbers_after_prefix(input: str, prefix: str) -> tuple[int, ...]:
+    return tuple(int(i) for i in input.removeprefix(prefix).strip().split())
+
+
+def parse(input: str) -> Races:
+    lines = [l for l in input.split("\n") if len(l) > 0]
+    assert len(lines) == 2, lines
+    times = get_numbers_after_prefix(lines[0], "Time:")
+    records = get_numbers_after_prefix(lines[1], "Distance:")
+    assert len(times) == len(records)
+    return tuple(Race(t, d) for t, d in zip(times, records))
+
+
+def part1(races: Races):
+    scores: list[int] = []
+    for race in races:
+        possible_speeds = np.arange(1, race.time)
+        possible_distances = possible_speeds * (race.time - possible_speeds)
+        is_record_break = possible_distances > race.distance_record
+        n_record_breaks = is_record_break.sum()
+        scores.append(n_record_breaks)
+    return reduce(lambda a, b: a * b, scores, 1)
+
+
+def part2(races: Races):
+    real_race: Race = Race(
+        time=int(reduce(lambda a, b: f"{a}{b}", [r.time for r in races], "")),
+        distance_record=int(
+            reduce(lambda a, b: f"{a}{b}", [r.distance_record for r in races], "")
+        ),
+    )
+    return part1((real_race,))
+
+
+if __name__ == "__main__":
+    input = load_input("input-06.txt")
+    races = parse(input)
+    print(f"Part 1: {part1(races)}")
+    print(f"Part 2: {part2(races)}")
